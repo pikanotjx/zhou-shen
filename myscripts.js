ef04c98427781e8cb6ed0ee59ba66441e961f685
pikanotjx <kwokjingxuan@gmail.com>
2022-11-25 12:49:11 +0800

    homepage updates

---
+++ myscripts.js
@@ -0,0 +1,5 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function openMenu() {
+    document.getElementById("mobile__menu").classList.toggle("overlay--active");
+  }
