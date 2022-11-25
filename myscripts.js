9722f2b5cd4000840546cbaa4e6267ccc0e442b6
pikanotjx <kwokjingxuan@gmail.com>
2022-11-25 18:33:32 +0800

    added music page

--- myscripts.js
+++ myscripts.js
@@ -1,5 +1,3 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
 function openMenu() {
     document.getElementById("mobile__menu").classList.toggle("overlay--active");
   }
