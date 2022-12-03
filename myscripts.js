fd6c91ea4e41db83c6d1f567b523e50cad582e17
pikanotjx <kwokjingxuan@gmail.com>
2022-12-03 20:02:04 +0800

    homepage edits

--- myscripts.js
+++ myscripts.js
@@ -1,3 +1,4 @@
+// navigation menu (header)
 function openMenu() {
-    document.getElementById("mobile__menu").classList.toggle("overlay--active");
-  }
+  document.getElementById("mobile__menu").classList.toggle("overlay--active");
+}
